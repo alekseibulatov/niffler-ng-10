9215ef3b3a6bf7781cd82625a9db526a654c46f8
alekseibulatov <bulatov2803@gmail.com>
2025-12-14 00:04:37 +0300

    add class

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/Spending.java
@@ -0,0 +1,22 @@
+package guru.qa.niffler.jupiter.annotation;
+
+import guru.qa.niffler.jupiter.extension.CreateSpendingExtension;
+import guru.qa.niffler.jupiter.extension.SpendingResolverExtension;
+import guru.qa.niffler.model.CurrencyValues;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@ExtendWith({CreateSpendingExtension.class, SpendingResolverExtension.class})
+public @interface Spending {
+    String username();
+    String category();
+    String description();
+    double amount();
+    CurrencyValues currency();
+}
---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/MainPage.java
@@ -0,0 +1,26 @@
+package guru.qa.niffler.page;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class MainPage {
+    private final SelenideElement spendingTable = $("#spendings");
+
+    public MainPage checkThatPageLoaded() {
+        spendingTable.should(visible);
+        return this;
+    }
+
+    public EditSpendingPage editSpending(String description) {
+        spendingTable.$$("tbody tr").find(text(description)).$$("td").get(5).click();
+        return new EditSpendingPage();
+    }
+
+    public MainPage checkThatTableContains(String description) {
+        spendingTable.$$("tbody tr").find(text(description)).should(visible);
+        return this;
+    }
+}
